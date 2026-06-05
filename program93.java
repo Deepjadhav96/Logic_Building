0b1a1e822686db32c69c65cc0d42e6488d131240
Deepraj <deepjadhav183@gmail.com>
2026-06-05 10:46:25 +0530

    Check palindrome

---
+++ program93.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+class DigitX
+{
+    public boolean CheckPalindrome(int iNo)
+    {
+        int iDigit = 0;
+        int iRev = 0;
+        int iTemp = 0;
+
+        iTemp = iNo;
+
+        while(iNo != 0)
+        {
+           iDigit =  iNo % 10;
+           
+           iRev  = (iRev * 10) + iDigit;
+
+           iNo = iNo / 10;
+           
+        }
+
+        if(iRev == iTemp)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
+
+class program93
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        DigitX dobj = new DigitX();
+
+
+        System.out.println("Enter a number:");
+
+        int iValue = 0;
+        boolean bRet = false ;
+
+        iValue = sobj.nextInt();
+
+       bRet = dobj.CheckPalindrome(iValue);
+
+       if(bRet == true)
+       {
+
+         System.out.println("Number is palindrome");
+
+       }
+       else
+       {
+         System.out.println("Number is not palindrome");
+       }
+    }
+}
