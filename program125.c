e3533863bdc6f82f2cff775913513f841d566b5a
Deepraj <deepjadhav183@gmail.com>
2026-06-09 23:31:10 +0530

    Sum of even number in Array

---
+++ program125.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int SumEven(int Arr[], int iSize)
+{
+
+    int iSum = 0;
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] % 2 == 0)
+        {
+            iSum = iSum + Arr[iCnt];
+        }
+    }
+
+    return iSum;
+
+
+}
+
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength,iCnt = 0;
+    int iRet = 0;
+
+    printf("Enter a number of element: \n");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(sizeof(int) * iLength); 
+
+    printf("Enter the elements:\n");
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+
+   iRet = SumEven(Brr, iLength);
+
+   printf("Summation of Even number is :%d \n",iRet);
+
+   free(Brr);
+
+    return 0;
+}
