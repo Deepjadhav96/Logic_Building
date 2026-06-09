d3f739eeb584e6f43f572101f12ba395828b2b58
Deepraj <deepjadhav183@gmail.com>
2026-06-09 23:36:05 +0530

    Linear Search of an array

---
+++ program128.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+bool LinearSearch(int Arr[], int iSize)
+{
+
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+       if(Arr[iCnt] == 11)
+       {
+        return true;    // Bad programming practice (Dont return true from for loop).
+       }
+    }
+
+    return false;
+}
+
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength,iCnt = 0;
+    bool bRet = false;
+
+    printf("Enter a number of element: \n");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(sizeof(int) * iLength); 
+
+    printf("Enter the elements:\n");
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+
+   bRet = LinearSearch(Brr, iLength);
+
+   if(bRet == true)
+   {
+    printf("Element is present \n ");
+   }
+   else
+   {
+    printf("Element is not present \n");
+   }
+
+   free(Brr);
+
+    return 0;
+}
