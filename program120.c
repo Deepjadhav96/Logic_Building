9248f4918963dc0d859064cd35f6a30dff9a31aa
Deepraj <deepjadhav183@gmail.com>
2026-06-09 23:24:46 +0530

    Dynamic memory allocation for an Array

---
+++ program120.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0, iCnt = 0;
+
+    //Step1: Accept the number of elements.
+    printf("Enter number of elements:");
+    scanf("%d",&iLength);
+
+    //Step2: Allocate the memory.
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    //Step3: Accpet the value from User.
+    printf("Enter number the elements: \n");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    //Step4:Use the memory(LOGIC).
+
+    //Step5: Deallocate the memory.
+
+    free(Brr);
+
+
+    return 0;
+}
+
