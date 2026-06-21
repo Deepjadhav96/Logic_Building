4d9421d5a41b925c81ca937074e3338c00b757e0
Deepraj <deepjadhav183@gmail.com>
2026-06-21 23:49:51 +0530

    Access specifier and Destructor

---
+++ program163.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+class ArrayX
+{
+    public:
+        int *Arr;
+        int iSize;
+
+        
+        ArrayX(int X)              
+        {
+            iSize = X;              
+
+            Arr = new int[iSize];   
+        }
+
+        ~ArrayX()                   
+        {
+            delete []Arr;           
+        }
+};
+
+int main()
+{
+    
+    ArrayX *aobj1 = new ArrayX(5);         
+    
+    //Logic (function call)
+
+    cout<<aobj1->iSize<<"\n";
+
+    aobj1 -> iSize = 0;
+    aobj1 -> NULL;       //This both line generate issue. 
+
+    cout<<aobj1->iSize<<"\n";
+    delete aobj1;
+
+
+   
+    return 0;
+}
