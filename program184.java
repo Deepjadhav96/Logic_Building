049cd8e1b8471861b19947d0e6bc92ebc3bf1600
Deepraj <deepjadhav183@gmail.com>
2026-06-26 23:57:22 +0530

    Sequential characters and stars pattern

---
+++ program184.java
@@ -0,0 +1,45 @@
+////////////////////////////////
+/////Input: 7
+/////
+/////Output: A * B * C * D 
+/////
+///////////////////////////////
+
+import java.util.*;
+
+class program181
+{
+    public static void Display(int iNo)
+    {
+        int iCnt = 0;
+        char ch = '\0';
+
+        for(iCnt = 1, ch = 'A'; iCnt <= iNo; iCnt++)
+        {
+            if(iCnt % 2 != 0)
+            {
+                System.out.print(ch+"\t");
+                ch++;
+            }
+            else
+            {
+                System.out.print("*\t");
+            }
+        }
+           
+        System.out.println();
+
+        
+    }
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue = 0;
+
+        System.out.println("Enter number of element:");
+        iValue = sobj.nextInt();
+
+        Display(iValue);
+
+    }
+}
