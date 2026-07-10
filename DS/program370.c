2cfb800bfa0a0c05c1bf9b16cd820da136b548e8
Deepraj <deepjadhav183@gmail.com>
2026-07-10 23:31:50 +0530

    Linked list template

---
+++ DS/program370.c
@@ -0,0 +1,59 @@
+//LinkdeList template
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void Display(PNODE first)
+{
+    
+}
+int Count(PNODE first)
+{
+    return 0;
+}
+
+//////////////
+
+void InsertFirst(PPNODE first, int iNo)
+{
+
+}
+void InsertLast(PPNODE first, int iNo)
+{
+
+}
+void InsertAtPos(PPNODE first, int iNo, int iPos)
+{
+
+}
+
+///////////////
+void DeleteFirst(PNODE first)
+{
+
+}
+void DeleteLast(PNODE first)
+{
+
+}
+void DeleteAtPos(PNODE first, int iPos)
+{
+
+}
+/////////////////
+
+int main()
+{
+    PNODE head = NULL;
+    return 0;
+}
