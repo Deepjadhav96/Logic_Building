1c919bf327c2c0a1ca49a11728a2da6edadcfdb7
Deepraj <deepjadhav183@gmail.com>
2026-07-15 22:29:09 +0530

    Linked list programs

---
+++ DS/program377.c
@@ -0,0 +1,115 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void Display(PNODE first)
+{
+    while(first)//same as (first != 0)
+    {
+        printf("| %d | ->",first->data);//Beautification of display function
+        first = first->next;
+    }
+    printf("NULL\n");
+    
+}
+int Count(PNODE first)
+{
+    int iCount = 0;
+    while (first)//same as (first != 0)  [Avoid this]
+    {
+        iCount++;
+        first = first -> next;
+    }
+    
+    return iCount;
+
+}
+
+//////////////
+
+void InsertFirst(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNo;
+    newn -> next = NULL;
+
+    if( NULL == *first) //LL is empty
+    {
+        *first = newn;
+
+
+    }
+    else                //LL contains atleast one node
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+
+}
+void InsertLast(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNo;
+    newn -> next = NULL;
+
+    if(*first == NULL) //LL is empty
+    {
+        *first = newn;
+
+    }
+    else                //LL contains atleast one node
+    {
+
+    }
+
+}
+void InsertAtPos(PPNODE first, int iNo, int iPos)
+{
+
+}
+
+///////////////
+void DeleteFirst(PNODE first)
+{
+
+}
+void DeleteLast(PNODE first)
+{
+
+}
+void DeleteAtPos(PNODE first, int iPos)
+{
+
+}
+/////////////////
+
+int main()
+{
+    PNODE head = NULL;
+    
+    InsertFirst(&head,101);
+    InsertFirst(&head,51);
+    InsertFirst(&head,21);
+    InsertFirst(&head,11);
+
+    Display(head);
+
+    int iRet = 0;
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n",iRet);
+
+    return 0;
+}
---
+++ DS/program382.c
@@ -0,0 +1,183 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void Display(PNODE first)
+{
+    while(first)//same as (first != 0)
+    {
+        printf("| %d | ->",first->data);//Beautification of display function
+        first = first->next;
+    }
+    printf("NULL\n");
+    
+}
+int Count(PNODE first)
+{
+    int iCount = 0;
+    while (first != 0)                      //type1
+    {
+        iCount++;
+        first = first -> next;
+    }
+    
+    return iCount;
+
+}
+
+//////////////
+
+void InsertFirst(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNo;
+    newn -> next = NULL;
+
+    if( NULL == *first) //LL is empty
+    {
+        *first = newn;
+
+
+    }
+    else                //LL contains atleast one node
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+
+}
+void InsertLast(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNo;
+    newn -> next = NULL;
+
+    if(NULL==*first) //LL is empty
+    {
+        *first = newn;
+
+    }
+    else                //LL contains atleast one node
+    {
+        temp = *first;
+        while(temp -> next != NULL)                             //type2
+        {
+            temp = temp -> next;
+        }
+        temp -> next = newn;
+    }
+
+}
+void InsertAtPos(PPNODE first, int iNo, int iPos)
+{
+
+}
+
+///////////////
+void DeleteFirst(PPNODE first)
+{
+            PNODE temp = NULL;
+
+
+    if(*first == NULL)      //LL is empty
+    {
+        return;
+    }
+    else if((*first) ->next == NULL)  //LL contains one node
+    {
+        free(*first);
+        *first = NULL;
+
+    }
+    else                //LL contains more than one node
+    {
+        temp = *first;
+
+        *first = (*first) -> next;
+
+        free(temp);
+
+    }
+
+}
+void DeleteLast(PPNODE first)
+{
+    PNODE temp = NULL;
+     if(*first == NULL)      //LL is empty
+    {
+        return;
+    }
+    else if((*first) ->next == NULL)  //LL contains one node
+    {
+        free(*first);
+        *first = NULL;
+
+    }
+    else                //LL contains more than one node
+    {
+        temp = *first;                                           //Type 3
+        while (temp -> next -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        
+        free(temp->next);
+        temp->next = NULL;
+
+    }
+
+}
+void DeleteAtPos(PPNODE first, int iPos)
+{
+
+}
+/////////////////
+
+int main()
+{
+    PNODE head = NULL;
+    
+    InsertFirst(&head,101);
+    InsertFirst(&head,51);
+    InsertFirst(&head,21);
+    InsertFirst(&head,11);
+
+    Display(head);
+
+    int iRet = 0;
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n",iRet);
+
+    InsertLast(&head , 111);
+    InsertLast(&head , 121);
+
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n",iRet);
+
+    DeleteFirst(&head);
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n",iRet);
+
+    DeleteLast(&head);
+     Display(head);    
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n",iRet);
+
+    return 0;
+}
