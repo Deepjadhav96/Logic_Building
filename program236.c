b8362383860ac75552b79d8775e24e3f1798edde
Deepraj <deepjadhav183@gmail.com>
2026-09-03 22:41:37 +0530

    Basics of string

---
+++ program236.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+
+    char *str = "Ganesh";        //char str[] = "JayGanesh"
+    
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    
+
+    return 0;
+}
