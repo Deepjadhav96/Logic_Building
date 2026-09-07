faecbdbcd694742c585cc603060ce1a404d0aba6
Deepraj <deepjadhav183@gmail.com>
2026-09-07 22:58:00 +0530

    Add frequncy of each character in Hasmap

---
+++ Advanced_LB/program774.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class program773
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        HashMap <Character,Integer> hobj = new HashMap<Character,Integer>();
+
+        System.out.println("Enter a string:");
+        String str = sobj.nextLine();
+
+        char Arr[] = str.toCharArray();
+        int iCount = 0;
+
+       for( char ch : Arr)           //for each loop
+       {
+            if(hobj.containsKey(ch))          //If its already exist
+            {
+                iCount = hobj.get(ch);      //old frequency
+                hobj.put(ch , iCount+1);   // add old freq + 1
+            }
+            else
+            {
+                hobj.put(ch , 1);
+            }
+
+       }
+
+       for(char cValue: hobj.keySet())
+       {
+            System.out.println(cValue+" Occurs "+hobj.get(cValue)+" times");
+       }
+
+       
+    }
+}
