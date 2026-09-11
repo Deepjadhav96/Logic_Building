73a1a65415c97f53f46726b97f600f21944cbb8b
Deepraj <deepjadhav183@gmail.com>
2026-09-11 23:47:41 +0530

    Cannonical function call

---
+++ System_Design/program988.java
@@ -0,0 +1,29 @@
+
+class Demo
+{
+    public int i,j;
+
+    public Demo fun()
+    {
+        return this;
+    }
+
+    public int gun()
+    {
+        return 11;
+    }
+}
+
+
+class program988
+{
+    public static void main(String A[])
+    {
+
+        Demo dobj  = new Demo();
+        
+        int ret = dobj.fun().gun();         //Canonical function call
+
+        System.out.println(ret);
+    }
+}
---
+++ System_Design/program989.java
@@ -0,0 +1,29 @@
+
+class Demo
+{
+    public int i,j;
+
+    public Demo fun()
+    {
+        this.i = 11;
+        return this;
+    }
+
+    public int gun()
+    {
+        return this.i;
+    }
+}
+
+class program989
+{
+    public static void main(String A[])
+    {
+
+        Demo dobj  = new Demo();
+        
+        int ret = dobj.fun().gun();         //Canonical function call
+
+        System.out.println(ret);
+    }
+}
