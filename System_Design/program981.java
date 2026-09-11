f37bae5944b4262772d307c962a4a16cdbf4b6f5
Deepraj <deepjadhav183@gmail.com>
2026-09-11 23:42:57 +0530

    Singleton and factory

---
+++ System_Design/program981.java
@@ -0,0 +1,34 @@
+class ParkingLot
+{
+    private static ParkingLot instance;                             //Self referential class
+
+    private ParkingLot()
+    {
+        System.out.println("Parking Lot object gets created");
+    }
+
+    public static ParkingLot getInstance()                          //getInstance() is Not an in built()
+    {
+        if(instance == null)
+        {
+            instance = new ParkingLot();
+        }
+
+        return instance;
+    }
+}
+
+class program981
+{
+    public static void main(String A[])
+    {
+        ParkingLot pobj1 = ParkingLot.getInstance();             
+
+        ParkingLot pobj2 = ParkingLot.getInstance();
+
+        ParkingLot pobj3 = ParkingLot.getInstance();
+
+
+    }
+}
+
