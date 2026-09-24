f817f179c02e5034314f5efac96337090f53b1fd
Deepraj <deepjadhav183@gmail.com>
2026-09-24 22:02:07 +0530

    Print a matrix

---
+++ Advanced_LB/program789.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class program789
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iRow = 0;
+        int iCol = 0;
+        int i = 0, j = 0;
+
+        System.out.println("Enter number of rows:");
+        iRow = sobj.nextInt();
+
+        System.out.println("Enter number of columns:");
+        iCol = sobj.nextInt();
+
+        int Arr[][] = new int[iRow][iCol];//Dynamic 2D array
+
+        System.out.println("Enter the elemnts of Matrix: ");
+
+        for(i = 0; i < iRow; i++ )
+        {
+            for(j = 0; j < iCol; j++)
+            {
+                Arr[i][j] = sobj.nextInt();
+            }
+        }
+        
+        System.out.println("Elemnts of Matrix: ");
+
+        for(i = 0; i < iRow; i++ )
+        {
+            for(j = 0; j < iCol; j++)
+            {
+                System.out.print(Arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
+
